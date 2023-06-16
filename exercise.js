91da5a5bbce666a8ad76c78cd424bf696f49fa9e
Luciano <lunick997@gmail.com>
2023-06-16 12:41:38 +0200

    fix

--- exercise.js
+++ exercise.js
@@ -1,5 +1,8 @@
 function sum(...rest) {
-    return rest;
+    let result = 0
+    rest.forEach(i => result + i);
+    return result;
 }
+// non capisco perchè non mi funzioni il programma, cosa sto sbagliando?
 
-console.log(sum(1, 2, 3, 4, 5,6,7,8,9,10,11,12,13,14));
+console.log(sum(1,2,3,4,5,6,7,8,9,10,11,12,13,14));
